fc9e0316c0ecccff1de4a8f87a6d38184a663230
Matthieu Michon <matthieu.michon@gmail.com>
2020-12-26 23:05:20 +0100

    Add part 2 of day 12

--- 2020/day-12/process.py
+++ 2020/day-12/process.py
@@ -13,6 +13,10 @@ import math
 
 
 TXT_FILES = [Path(f) for f in os.listdir('./') if '.txt' in f]
+WAYPOINT_START_OFFSET = {
+    'east': 10,
+    'north': 1
+}
 
 
 def process(file: Path) -> int:
@@ -52,6 +56,58 @@ def process(file: Path) -> int:
     return manhattan_distance
 
 
+def process_part2(file: Path) -> int:
+    """
+    Process input file yielding the submission value
+
+    :param file: file containing the input values
+    :return: value to submit
+    """
+
+    waypoint_offset = dict(WAYPOINT_START_OFFSET)
+    ship_position = {'east': 0, 'north': 0}
+
+    rad = lambda d: d / 360 * 2 * math.pi
+
+    instructions = [l.strip() for l in open(file)]
+    for i in instructions:
+        action = i[0]
+        value = int(i[1:])
+        if action == 'R':
+            east = round(
+                waypoint_offset['east'] * math.cos(rad(value)) + \
+                waypoint_offset['north'] * math.sin(rad(value)))
+            north = round(
+                waypoint_offset['north'] * math.cos(rad(value)) - \
+                waypoint_offset['east'] * math.sin(rad(value)))
+            waypoint_offset['east'] = east
+            waypoint_offset['north'] = north
+        elif action == 'L':
+            east = round(
+                waypoint_offset['east'] * math.cos(rad(value)) - \
+                waypoint_offset['north'] * math.sin(rad(value)))
+            north = round(
+                waypoint_offset['north'] * math.cos(rad(value)) + \
+                waypoint_offset['east'] * math.sin(rad(value)))
+            waypoint_offset['east'] = east
+            waypoint_offset['north'] = north
+        if action == 'E':
+            waypoint_offset['east'] += value
+        if action == 'W':
+            waypoint_offset['east'] -= value
+        if action == 'N':
+            waypoint_offset['north'] += value
+        if action == 'S':
+            waypoint_offset['north'] -= value
+        if action == 'F':
+            ship_position['east'] += value * waypoint_offset['east']
+            ship_position['north'] += value * waypoint_offset['north']
+
+    manhattan_distance = int(abs(ship_position['east']) + abs(ship_position['north']))
+
+    return manhattan_distance
+
+
 def main() -> int:
     """
     Main function
@@ -63,14 +119,12 @@ def main() -> int:
         submission = process(file=Path(file))
         print(f'In file {file}, submission: {submission}')
 
+    print('Part 2')
+
+    for file in TXT_FILES:
+        submission = process_part2(file=Path(file))
+        print(f'In file {file}, submission: {submission}')
     return 0
-    #
-    # print('Part 2')
-    #
-    # for file in TXT_FILES:
-    #     submission = process_part2(file=Path(file))
-    #     print(f'In file {file}, submission: {submission}')
-    # return 0
 
 
 if __name__ == '__main__':
